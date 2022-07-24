841710899edc1a9bcea84378b61ece78a3cdd6e8
ILLIDOM <illi.dominique.v@gmail.com>
2022-07-24 16:37:52 +0200

    added user tests

--- application/tests/functional_tests/test_user.py
+++ application/tests/functional_tests/test_user.py
@@ -1,10 +1,19 @@
-def test_register(client, db):
+def test_valid_register(client, db):
     response = client.post('/register', json={
         "username": "Hans",
         "password": "mypassword"
     })
     assert response.status_code == 201
     assert response.json["message"] == 'User created successfully.'
+
+
+def test_invalid_register_user_already_exists(client, db):
+    response = client.post('/register', json={
+        "username": "Hans",
+        "password": "mypassword"
+    })
+    assert response.status_code == 400
+    assert response.json["message"] == 'A user with that username already exists'
 
 
 def test_valid_login(client, db):
